e1ebdff3f0a477f9a5e54a3698d972f0ca920c3c
dllo <dllo@sb.local>
2016-07-11 19:19:44 +0800

    创建工程

---
+++ TravelDemo/app/src/main/java/com/wyl/traveldemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wyl.traveldemo;
+//蝉游记
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
